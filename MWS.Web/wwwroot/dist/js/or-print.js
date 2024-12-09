255b280058afacdc5b7677e3da75a25ff9658949
Jok Garcia <ground.guru.jok@outlook.com>
2024-12-09 13:54:20 +0800

    LGU Onsite

--- MWS.Web/wwwroot/dist/js/or-print.js
+++ MWS.Web/wwwroot/dist/js/or-print.js
@@ -8,7 +8,7 @@
                 margin: 0.1,
                 filename: 'OR.pdf',
                 image: { type: 'jpeg', quality: 0.98 },
-                jsPDF: { unit: 'in', format: [8.5, 11], orientation: 'portrait' }
+                jsPDF: { unit: 'in', format: [8.5, 11], orientation: 'portrait'  }
             };
             html2pdf().from(invoice).set(opt).save();
         })
